5d86a3b1abd754622f14c94a191a80dfb473e0cf
almeida <jpedro.amsilva@gmail.com>
2023-03-03 22:00:25 +0000

    fix the send code

--- opencv.oilpipes-master/coderasp.py
+++ opencv.oilpipes-master/coderasp.py
@@ -1,8 +1,8 @@
-import cv2 as cv
-import numpy as np 
+#import cv2 as cv
+#import numpy as np 
 import serial
-from PIL import Image
-import os
+#from PIL import Image
+#import os
 
 # captura e armazenamento em uma variavel
 #webcam = cv.VideoCapture(0)
@@ -10,16 +10,14 @@ import os
 #cv.imwrite("Foto.png", frame)
 
 # transformando em matriz string
-imgPIL = Image.open("novin.png")
-arr = np.asarray(imgPIL)
-img_enc = str(arr)
+#imgPIL = Image.open("novin.png")
+#arr = np.asarray(imgPIL)
+#img_enc = str(arr)
 #print(img_enc)
 
 # envia matriz para esp
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-#ser.write(img_enc.encode('UTF-8'))
+ser.write(b'\x0101')
 
-#os.remove("Foto.png")
-
-cv.waitKey(0)
-cv.destroyAllWindows()
+#cv.waitKey(0)
+#cv.destroyAllWindows()
