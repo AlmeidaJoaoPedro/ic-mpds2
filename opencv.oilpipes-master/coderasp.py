f8f06cc938be17aa3ae884fe178126020bbc4af2
almeida <jpedro.amsilva@gmail.com>
2023-03-09 02:40:17 +0000

    funcionou poha

--- opencv.oilpipes-master/coderasp.py
+++ opencv.oilpipes-master/coderasp.py
@@ -1,23 +1,34 @@
-#import cv2 as cv
-#import numpy as np 
+#as bibliotecas nao estao apresentando mais problemas de importacao
+import cv2
+import numpy as np 
 import serial
-#from PIL import Image
-#import os
+from PIL import Image
+import os
 
-# captura e armazenamento em uma variavel
-#webcam = cv.VideoCapture(0)
-#ret, frame = webcam.read()
-#cv.imwrite("Foto.png", frame)
+#img_path = r"ic-mpds2/opencv.oilpipes-master/Foto.png"
+#directory = r"ic-mpds2/opencv.oilpipes-master"
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+#captura e armazenamento em uma variavel
+webcam = cv2.VideoCapture(0)
+ret, frame = webcam.read()
+os.chdir("ic-mpds2/opencv.oilpipes-master")
+cv2.imwrite("Foto.png", frame)
+
+#img_path = r"ic-mpds2/opencv.oilpipes-master/Foto.png"
 
 # transformando em matriz string
-#imgPIL = Image.open("novin.png")
-#arr = np.asarray(imgPIL)
-#img_enc = str(arr)
-#print(img_enc)
+Path1 = os.path.normpath("Foto.png")
+imgPIL = cv2.imread(Path1)
 
-# envia matriz para esp
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-ser.write(b'\x0101')
+arr = np.asarray(imgPIL)
+img_enc = str(arr)
 
-#cv.waitKey(0)
-#cv.destroyAllWindows()
+# envia matriz 
+
+ser.write(img_enc.encode('UTF-8'))
+os.remove("Foto.png")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
