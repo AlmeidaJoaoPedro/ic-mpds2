694e25168600e1c0ad325f15a1dae8d344cb87a2
RodrigofrBastos <rodrigo.bastos@aln.senaicimatec.edu.br>
2023-05-26 12:01:53 -0300

    tests from 26/05

--- test_from_26-05/image_capture.py
+++ test_from_26-05/image_capture.py
@@ -8,30 +8,31 @@ def switch_case(key):
         '1': "test_from_26-05/1m/leak_1m.jpg",
         '2': "test_from_26-05/10m/leak_10m.jpg",
         '3': "test_from_26-05/25m/leak_25m.jpg",
-        '4': "test_from_26-05/50m/leak_50m.jpg",
+        '4': "test_from_26-05/40m/leak_40m.jpg",
         '5': "test_from_26-05/100m/leak_100m.jpg"
     }
     return switch_dict.get(key, None)
 
 
 # Open the default camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
+
 # Read frames from the camera
 while(True):
     ret, frame = cap.read()
+    # resize = cv2.resize(frame,)
 
     cv2.imshow('frame',frame)
-# pressione a tecla "l" para tirar uma foto do vazamento
+# pressione a tecla 1 para foto em 1 metro
+# pressione a tecla 2 para foto em 10 metros
+# pressione a tecla 3 para foto em 25 metros
+# pressione a tecla 4 para foto em 50 metros
+# pressione a tecla 5 para foto em 100 metros
 
     key = chr(cv2.waitKey(1) & 0xFF)
     filename = switch_case(key)
     if filename:
         break
-
-
-
-
-# voce deve segurar a tecla "n" por alguns instantes para salvar a imagem sem vazamento
 
 
 cap.release()
@@ -50,7 +51,7 @@ if os.path.exists(filename):
     index = 1
     while os.path.exists(f"{os.path.splitext(filename)[0]}-{index}{os.path.splitext(filename)[1]}"):
         index += 1
-    filename = f"{os.path.splitext(filename)[0]}({index}){os.path.splitext(filename)[1]}"
+    filename = f"{os.path.splitext(filename)[0]}-{index}{os.path.splitext(filename)[1]}"
 
 
 # Agora, filename contém um nome de arquivo exclusivo
